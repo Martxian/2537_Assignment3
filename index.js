9e9ab5761770e9afee326146902731358ca0a6e2
Marvin Sio <marvinbcit@gmail.com>
2023-05-15 14:18:23 -0700

    Step 5.1 Pagination - Display at max 5 pagination buttons with next and previous buttons

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
-const setup = async () => {
+const PAGE_SIZE = 10
+let currentPage = 1;
+let pokemons = []
+
+// Pagination function to update pagination buttons
+const updatePaginationDiv = (currentPage, numPages) => {
+  // Clear the pagination div before adding new pagination buttons
+  $('#pagination').empty()
+
+  // Maximum number of pages to show in the pagination bar
+  const maxPagesToShow = 5;
+
+  // Calculate the start and end page numbers to show in the pagination bar
+  let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+  const endPage = Math.min(numPages, currentPage + Math.floor(maxPagesToShow / 2));
+
+  // If the current page is not the first page, add a 'Prev' button to go back to the previous page
+  if (currentPage > 1) {
+    $('#pagination').append(`
+      <button class="btn btn-primary page ml-1 numberedButtons" value="${currentPage - 1}">&laquo; Prev</button>
+    `);
+  }
+
+  // Add numbered buttons for the pages between startPage and endPage, and add the 'active' class to the current page button
+  for (let i = startPage; i <= endPage; i++) {
+    $('#pagination').append(`
+      <button class="btn btn-primary page ml-1 numberedButtons ${i == currentPage ? 'active' : ''}" value="${i}">${i}</button>
+    `)
+  }
+
+  // If the current page is not the last page, add a 'Next' button to go to the next page
+  if (currentPage < numPages) {
+    $('#pagination').append(`
+      <button class="btn btn-primary page ml-1 numberedButtons" value="${currentPage + 1}">Next &raquo;</button>
+    `);
+  }
+}
+
+const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
+
+  // get selected pokemons based on current page and page size
+  selected_pokemons = pokemons.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE)
+
+  // clear out existing pokemon cards
   $('#pokeCards').empty()
-  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
-  const pokemons = response.data.results;
-  pokemons.forEach(async (pokemon) => {
+
+  // loop through selected pokemons and add cards to page
+  selected_pokemons.forEach(async (pokemon) => {
     const res = await axios.get(pokemon.url)
     $('#pokeCards').append(`
-    <div class="pokeCard card" pokeName=${res.data.name}  >
-    <h3>${res.data.name.toUpperCase()}</h3> 
-    <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
-      More
-    </button>
-    </div> 
-        `)
+        <div class="pokeCard card" pokeName=${res.data.name}   >
+          <h3>${res.data.name.toUpperCase()}</h3> 
+          <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
+            More
+          </button>
+        </div>  
+      `)
   })
+
+  // update total pokemons counter
+  $('#totalPokemons').html(
+    `<h1>Viewing ${selected_pokemons.length} of ${pokemons.length} Pokemon </h1>`
+  );
+}
+
+const setup = async () => {
+  $('#pokeCards').empty();
+  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+  pokemons = response.data.results;
+
+  const numPages = Math.ceil(pokemons.length / PAGE_SIZE)
+
+  paginate(currentPage, PAGE_SIZE, pokemons)
+  updatePaginationDiv(currentPage, numPages)
 
   // pop up modal when clicking on a pokemon card
   // add event listener to each pokemon card
@@ -53,6 +112,15 @@ const setup = async () => {
           <h2>${res.data.name.toUpperCase()}</h2>
           <h5># ${res.data.id}</h5>
           `)
+  })
+
+  // add event listener to pagination buttons
+  $('body').on('click', ".numberedButtons", async function (e) {
+    currentPage = Number(e.target.value)
+    paginate(currentPage, PAGE_SIZE, pokemons)
+
+    //update pagination buttons
+    updatePaginationDiv(currentPage, numPages)
   })
 }
 
